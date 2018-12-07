4fa3d02aa7e67a8843a4f026a4fd55a64e18383c
Tim Rasim <rasim@imis.uni-luebeck.de>
2018-12-07 12:35:30 +0100

    Added seperate REST-request for grid overview

--- routes/tdm.js
+++ routes/tdm.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 
+/*
+ *  Returns mid-current grid with references to all subnets
+ */
+router.use('/overview', function (req, res, next) {
+    console.log('Request Id:', req.params.id);
+    res.download('./public/topology/mv-topology.json')
+});
+
+
+/*
+ *  Returns the subnet with a given ID
+ */
 router.use('/:id', function (req, res, next) {
     console.log('Request Id:', req.params.id);
     res.download('./public/topology/' + req.params.id + '.json')
