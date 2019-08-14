93dbbe634fd54f27d1620eae46aecd16566465aa
Tim Rasim <rasim@imis.uni-luebeck.de>
2019-08-14 16:38:06 +0200

    added: test topology for easier debugging

--- routes/tdm.js
+++ routes/tdm.js
@@ -33,6 +33,55 @@ router.use('/overview', function (req, res, next) {
 });
 
 /*
+ *  Returns mid-current grid with references to all subnets
+ */
+router.use('/test/overview', function (req, res, next) {
+
+    let topologyList = [];
+
+    //requiring path and fs modules
+    const path = require('path');
+    const fs = require('fs');
+    //joining path of directory
+    const directoryPath = path.join(__dirname, '../public/test_topology');
+    //passsing directoryPath and callback function
+    fs.readdir(directoryPath, function (err, files) {
+        //handling error
+        if (err) {
+            console.log('Unable to scan directory: ' + err);
+        } else {
+            //listing all files using forEach
+            files.forEach(function (filename) {
+                topologyList.push(filename.replace(/\.[^/.]+$/, ""));
+            });
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({content: JSON.stringify(topologyList)}));
+        }
+    });
+});
+
+
+/*
+ *  Returns the subnet with a given ID
+ */
+router.use('/test/:id', function (req, res, next) {
+    console.log('Request Id:', req.params.id);
+    //res.download('./public/topology/' + req.params.id + '.json')
+
+    var fs = require('fs');
+    fs.readFile( __dirname + '/../public/test_topology/' + req.params.id + '.json','utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({ error: {code: 404, message: 'Subnet with id: ' + req.params.id + ' could not be retrieved'}}));
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(JSON.stringify({content: data}));
+        }
+    });
+});
+
+/*
  *  Returns the subnet with a given ID
  */
 router.use('/:id', function (req, res, next) {
